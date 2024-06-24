580ea6eccfadbc29fb9a4a6c19d7766c3bade8aa
Sheriff Alidu <sheriffalidu1@gmail.com>
2024-06-24 18:46:17 -0400

    api_buiding

--- airquality.py
+++ airquality.py
@@ -1,2 +1,17 @@
-def my():
-    pass
+import requests
+import json
+
+API_TOKEN = 'your_api_token_here'
+CITY = 'your_city_here'
+
+# Fetch data from the API
+url = f'https://api.waqi.info/feed/{CITY}/?token={API_TOKEN}'
+response = requests.get(url)
+data = response.json()
+
+# Extract relevant data
+if data['status'] == 'ok':
+    weather_data = data['data']['iaqi']
+    aqi = data['data']['aqi']
+else:
+    print('Failed to fetch data')
